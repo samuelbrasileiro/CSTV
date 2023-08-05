1e93dab9a8eef2ec0ae4108b43e6bd340b4173d5
Samuel <samuelbsantosn@gmail.com>
2023-08-05 00:31:57 -0300

    feat: Register Match Tracker Service

--- MatchTracker/MatchTracker/Sources/Domain/DependencyInjection/MatchTrackerDomainAssemby.swift
+++ MatchTracker/MatchTracker/Sources/Domain/DependencyInjection/MatchTrackerDomainAssemby.swift
@@ -9,9 +9,18 @@ import Service
 
 class MatchTrackerDomainAssembly: Assembly {
     open func assemble(container: DependencyContainer) {
+        registerMatchTrackerService(container)
+        
         registerPlayersProvider(container)
     }
 
+    private func registerMatchTrackerService(_ container: DependencyContainer) {
+        container.register(MatchTrackerServiceProtocol.self) { resolver in
+            let networkService = resolver.resolve(NetworkServiceProtocol.self)
+            return MatchTrackerService(networkService: networkService)
+        }
+    }
+    
     private func registerPlayersProvider(_ container: DependencyContainer) {
         container.register(PlayersProviderProtocol.self) { resolver in
             let service = resolver.resolve(MatchTrackerServiceProtocol.self)
