7cca48ece667988b2d1547ca0809d4754e715441
Samuel <samuelbsantosn@gmail.com>
2023-08-05 11:44:53 -0300

    feat: Add property conversion from UIFont to Font

---
+++ Commons/Sources/Commons/Extensions/UIFont+swiftUIFont.swift
@@ -0,0 +1,14 @@
+//
+//  UIFont+swiftUIFont.swift
+//  
+//
+//  Created by Samuel Brasileiro on 05/08/23.
+//
+
+import SwiftUI
+
+public extension UIFont {
+    var swiftUIFont: Font {
+        return Font(self)
+    }
+}
