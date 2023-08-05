71f89ff08f441285cbd1e474654f0083170af317
Samuel <samuelbsantosn@gmail.com>
2023-08-05 15:43:37 -0300

    build: Remove unused CommonsTest target

--- Commons/Package.swift
+++ Commons/Package.swift
@@ -21,9 +21,6 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "Commons",
-            dependencies: []),
-        .testTarget(
-            name: "CommonsTests",
-            dependencies: ["Commons"]),
+            dependencies: [])
     ]
 )
