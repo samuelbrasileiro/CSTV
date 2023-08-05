d331b6e915eabb4f6d9969d4fb5429753913abe9
Samuel <samuelbsantosn@gmail.com>
2023-08-05 00:44:25 -0300

    feat: Create Team View

---
+++ MatchTracker/MatchTracker/Sources/UI/Components/Views/Match/TeamView.swift
@@ -0,0 +1,32 @@
+//
+//  TeamView.swift
+//  MatchTracker
+//
+//
+
+import Core
+import SDWebImageSwiftUI
+import SwiftUI
+
+struct TeamView: View {
+    var team: Opponent?
+    
+    var body: some View {
+        VStack(spacing: 10) {
+            WebImage(url: URL(string: team?.imageURL ?? ""))
+                .placeholder {
+                    CoreImage.unavailableImage.image
+                        .resizable()
+                        .aspectRatio(contentMode: .fill)
+                        .frame(width: 60, height: 60)
+                        .clipShape(Circle())
+                }
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(width: 60, height: 60)
+            Text(team?.name ?? "Sem Nome")
+                .font(CoreFont.roboto.regular(size: 10))
+                .foregroundColor(CoreColor.primary.color)
+        }
+    }
+}
