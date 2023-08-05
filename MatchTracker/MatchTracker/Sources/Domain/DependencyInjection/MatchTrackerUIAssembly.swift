b8c072c0f000129777ee061f7754dbf9eef6a666
Samuel <samuelbsantosn@gmail.com>
2023-08-05 07:02:01 -0300

    fix: Register ViewModels in UI Asembly

--- MatchTracker/MatchTracker/Sources/Domain/DependencyInjection/MatchTrackerUIAssembly.swift
+++ MatchTracker/MatchTracker/Sources/Domain/DependencyInjection/MatchTrackerUIAssembly.swift
@@ -9,6 +9,24 @@ import DependencyInjection
 
 class MatchTrackerUIAssembly: Assembly {
     func assemble(container: DependencyContainer) {
-        
+        registerMatchesViewModel(container)
+        registerMatchDetailsViewModel(container)
+    }
+
+    private func registerMatchesViewModel(_ container: DependencyContainer) {
+        container.register(MatchesViewModel.self) { (container) in
+            let upcomingMatchesProvider = container.resolve(UpcomingMatchesProviderProtocol.self)
+            let runningMatchesProvider = container.resolve(RunningMatchesProviderProtocol.self)
+            return MatchesViewModel(upcomingMatchesProvider: upcomingMatchesProvider,
+                                    runningMatchesProvider: runningMatchesProvider)
+        }
+    }
+    
+    private func registerMatchDetailsViewModel(_ container: DependencyContainer) {
+        container.register(MatchDetailsViewModel.self) { (resolver: DependencyResolver, match: Match) in
+            let playersProvider = container.resolve(PlayersProviderProtocol.self)
+            return MatchDetailsViewModel(match: match,
+                                         playersProvider: playersProvider)
+        }
     }
 }
